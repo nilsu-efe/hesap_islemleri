c567377116f124d6f4c39d6171abfe9b0fb0359f
Nilsu EFE <149186837+nilsu-efe@users.noreply.github.com>
2024-02-22 17:06:20 +0300

    Add files via upload

---
+++ test.java
@@ -0,0 +1,40 @@
+
+package constructor;
+
+
+public class test {
+    public static void main(String[] args) {
+        
+        /*account account=new account();
+        
+        account.setIsim("nilsu");
+        account.setBakiye(1000.0);
+        account.setEmail("nilsu.05");
+        account.setHesapno("123456");
+        account.setTelno("505211");
+        
+        System.out.println(""+account.getIsim());
+        account.paraYatirma(50);
+        account.paraCekme(500);
+        account.paraCekme(1500);*/
+        
+        /*account account1=new account("123456", 1000,"nilsu","nlsu.05","505211");
+        
+        account1.paraYatirma(500);
+        account1.paraCekme(1501);
+        account1.paraCekme(300);*/
+        
+        /*account account2=new account("nilsu", "nilsuu.05", "050211");
+        
+        /*System.out.println(account2.getIsim());
+        System.out.println(account2.getEmail());
+        System.out.println(account2.getTelno());
+        System.out.println(account2.getBakiye());
+        System.out.println(account2.getHesapno());*/
+        
+        //account2.bilgiver();*/
+        
+        
+    }
+    
+}
